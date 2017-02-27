3df0efd49ffe6192b32a45094726ba59327465c0
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-02-27 11:43:00 -0800

    refator app using express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT)
+console.log('Listening on 4000');
